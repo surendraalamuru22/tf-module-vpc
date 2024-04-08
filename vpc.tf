e5fd2bcc3e0dd75049c53a130623b1db2565ed57
surendra <surendraredddyalamuru01@gmail.com>
2024-04-08 19:25:58 +0530

    vpc

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,6 @@ resource "aws_vpc" "main" {
 
 module "additional_vpc_cidr" {
   for_each = var.vpc
-  env = var.env
   source = "./additional_vpc_cidr"
   additional_cidr_block = each.value.additional_cidr_block
   vpc_id = [for k, v in aws_vpc.main : v.id]
