0d7447e056e28baa26f4ee7fc05df31d07da1a3d
surendra <surendraredddyalamuru01@gmail.com>
2024-04-16 10:07:35 +0530

    route

--- peering.tf
+++ peering.tf
@@ -5,3 +5,11 @@ resource "aws_vpc_peering_connection" "management-vpc-to-dev-vpc" {
   auto_accept = true
   tags = local.vpc_peering_tags
 }
+
+
+resource "aws_route" "route-to-default-management-route-table" {
+  for_each = var.vpc
+  route_table_id            = lookup(var.management_vpc, "route_table_id", null)
+  destination_cidr_block    = each.value.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-dev-vpc.*.id[0]
+}
