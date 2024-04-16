fcab4181175462800526d2cd7d398cee7aa6e1f2
surendra <surendraredddyalamuru01@gmail.com>
2024-04-16 09:47:20 +0530

    route

--- subnets/main.tf
+++ subnets/main.tf
@@ -26,3 +26,11 @@ output "subnets" {
 output "route-table" {
   value = aws_route_table.aws-route-table
 }
+
+
+resource "aws_route" "peering-connection-route" {
+  for_each = var.subnets
+  route_table_id            = lookup(lookup(aws_route_table.aws-route-table, each.value.name, null), "id", null)
+  destination_cidr_block    = lookup(var.management_vpc, "cidr_block", null)
+  vpc_peering_connection_id = var.peering_connection_id
+}
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -2,3 +2,5 @@ variable "subnets" {}
 variable "vpc_id" {}
 variable "env" {}
 variable "subnet_availability_zones" {}
+variable "management_vpc" {}
+variable "peering_connection_id" {}
