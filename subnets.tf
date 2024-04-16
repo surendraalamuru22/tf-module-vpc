49bff0cf889de356edccf599e564a4092cacdbed
surendra <surendraredddyalamuru01@gmail.com>
2024-04-16 16:41:12 +0530

    igw

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,19 @@
-module "subnets" {
+module "public_subnets" {
   for_each = var.vpc
   source = "./subnets"
-  subnets = each.value.subnets
+  subnets = each.value.public_subnets
+  vpc_id = [for k, v in aws_vpc.main : v.id]
+  env = var.env
+  subnet_availability_zones = each.value.subnet_availability_zones
+  peering_connection_id = aws_vpc_peering_connection.management-vpc-to-dev-vpc.*.id[0]
+  management_vpc = var.management_vpc
+}
+
+
+module "private_subnets" {
+  for_each = var.vpc
+  source = "./subnets"
+  subnets = each.value.pivate_subnets
   vpc_id = [for k, v in aws_vpc.main : v.id]
   env = var.env
   subnet_availability_zones = each.value.subnet_availability_zones
