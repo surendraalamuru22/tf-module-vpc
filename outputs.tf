e705fce2ea9423d750122aa77adcd8164ead82b6
surendra <surendraredddyalamuru01@gmail.com>
2024-04-18 14:04:54 +0530

    igw

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
+output "private_subnet" {
+  value = module.private_subnets
+}
 
+output "public_subnet" {
+  value = module.public_subnets
+}
---
+++ subnets/outputs.tf
@@ -0,0 +1,24 @@
+locals {
+  subnets_list = flatten([for i, j in module.lm-subnets : j.subnets])
+}
+
+output "subnets_list" {
+  value = local.subnets_list[*].id
+}
+
+
+output "subnets" {
+  value = module.lm-subnets
+}
+
+output "rt" {
+   value = [for i, j in aws_route_table.aws_route_table : j.id]
+//  //{
+//  //    for k, v in aws_route_table.aws_route_table : k => v.id
+//  //  }
+}
+
+
+#output "route_tables" {
+#  value = aws_route_table.aws_route_table
+#}
