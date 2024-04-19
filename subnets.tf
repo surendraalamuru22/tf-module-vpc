80429c3f15a7075b4a01ab661ccd413a663a8ca9
surendra <surendraredddyalamuru01@gmail.com>
2024-04-19 14:09:09 +0530

    subnets

--- subnets.tf
+++ subnets.tf
@@ -11,14 +11,16 @@ module "public_subnets" {
 #  management_vpc = var.management_vpc
 }
 
-#
-#module "private_subnets" {
-#  for_each = var.vpc
-#  source = "./subnets"
-#  subnets = each.value.private_subnets
-#  vpc_id = [for k, v in aws_vpc.main : v.id]
-#  env = var.env
-#  subnet_availability_zones = each.value.subnet_availability_zones
+
+module "private_subnets" {
+  for_each = var.private_subnets
+  source = "./subnets"
+  subnets = var.private_subnets
+  cidr_block = each.value.cidr_block
+  name = each.value.name
+  vpc_id = aws_vpc.main.id
+  env = var.env
+  subnet_availability_zones = var.subnet_availability_zones
 #  peering_connection_id = aws_vpc_peering_connection.management-vpc-to-dev-vpc.*.id[0]
 #  management_vpc = var.management_vpc
-#}
+}
